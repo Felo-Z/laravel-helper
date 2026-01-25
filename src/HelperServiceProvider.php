3b05a67c908644443beb203511b0edabae543ec4
zhangfei <15381493053@qq.com>
2026-01-25 16:47:50 +0800

    Fix: 修复配置文件发布路径问题

--- src/HelperServiceProvider.php
+++ src/HelperServiceProvider.php
@@ -7,21 +7,17 @@ use Illuminate\Support\ServiceProvider;
 class HelperServiceProvider extends ServiceProvider
 {
     protected bool $defer = true;
+
     /**
      * Perform post-registration booting of services.
      */
     public function boot(): void
     {
         $this->publishes([
-            \dirname(__DIR__).'/migrations/' => database_path('migrations'),
-        ], 'migrations');
-
-        $this->publishes([
-            \dirname(__DIR__).'/publish/felo-helper.php' => config_path('felo-helper.php'),
+            __DIR__.'/publish/felo-helper.php' => config_path('felo-helper.php'),
         ], 'config');
 
         if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(\dirname(__DIR__).'/migrations/');
             $this->commands([
                 \FeloZ\LaravelHelper\Commands\ClearLogsCommand::class,
                 \FeloZ\LaravelHelper\Commands\ClearCacheCommand::class,
